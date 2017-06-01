d5577b8fa14ef95577bdb507559a3e7a42abcabf
Rafael Zomer Antunes <rafaelzomer@gmail.com>
2017-05-31 21:18:44 -0300

    maquina ok

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include "maquina.h"
 #include "operacoes/multiplicacao.h"
+#include "operacoes/subtracao.h"
 
 using namespace std;
 
@@ -17,6 +18,7 @@ void menu(){
 	cout << "2 - Subtracao" << endl;
 	cout << "3 - Divisao" << endl;
 	cout << "4 - Multiplicacao" << endl;
+	cout << "5 - Entrar com maquina" << endl;
 	cout << "0 - Sair" << endl;
 	cout <<endl;
 }
@@ -30,37 +32,51 @@ void entradados(vector<string> *dados) {
 		
 		
 		if(dado.compare("0")){
-			dados->push_back(dado);
-			
-//			lin = dados[0];
-//			col = dados[2];
-//			es.proximoEstado = dados[4];
-//			es.escreve = dados[6];
-//			es.direcaoMovimento = dados[8];
-//			
-//			linhas[lin][col] = es;	
+			dados->push_back(dado);	
 		}
 		
 	}
 	while(dado.compare("0"));
 }
 
+void entra(string titulo, string *fita) {
+	cout << "Informe "<< titulo << ": " << endl;
+	cin >> *fita; 
+}
+
 void programa() {
-	int opcao;
+	int opcao;	
+	char inicial = 'K';
+	char final = 'F';
+	string fita, ini, fim;
+	maquina *maq;
+	
 	vector<string> *dados = new vector<string>();
-	maquina *maq = new maquina();
 	do{
 		menu();
         cin >> opcao;
         
         switch(opcao){
-           	case 1:
-              entradados(dados);
-              break;  
+			case 2:
+				subtracao::gerar(dados, &inicial, &final);
+				maq = new maquina(dados, inicial, final);
+				entra("fita", &fita);
+				maq->ler(fita);
+				break;   
 			case 4:
-              multiplicacao::gerar(dados);
-              maq->ler(dados);
-              break;        
+				multiplicacao::gerar(dados, &inicial, &final);
+				maq = new maquina(dados, inicial, final);
+				entra("fita", &fita);
+				maq->ler(fita);
+				break;
+           	case 5:
+              	entradados(dados);
+            	entra("inicial", &ini);
+            	entra("final", &fim);
+				maq = new maquina(dados, ini[0], fim[0]);
+            	entra("fita", &fita);
+	            maq->ler(fita);
+	            break;         
     	}
 		system("PAUSE"); 
     } while(opcao != 0);
@@ -73,94 +89,6 @@ int main()
     programa();  
     return 0;
 }
-
-//struct estado{
-//	char proximoEstado, escreve, direcaoMovimento; 
-//};
-//
-//map<char, map<char, estado> > linhas;
-//string fita;
-//int i; 
-//
-//void ler(char est, char simbolo){
-//	estado es = linhas[est][simbolo];
-//	
-//	char proxSim;
-//	if(es.direcaoMovimento == 'D') {
-//		proxSim = fita[++i];
-//	} else {
-//		proxSim = fita[--i];	
-//	}
-//	fita[i] = es.escreve;
-//	
-//	cout << fita;
-//	
-//	ler(es.proximoEstado, proxSim);	
-//}
-//
-//void entraDados(){
-//	
-//	string dados;
-//	estado es;
-//	char lin, col, simboloIni;
-//   
-//	
-//	do{
-//		cout << "Informe o estado: " << endl;	
-//		cout << "(Padrao: 0,A=1,A,D)" << endl;
-//		cin >> dados;
-//		
-//		
-//		if(dados.compare("0")){
-//			//d.push_back(dados);              0,A=1,A,D
-//			
-//			lin = dados[0];
-//			col = dados[2];
-//			es.proximoEstado = dados[4];
-//			es.escreve = dados[6];
-//			es.direcaoMovimento = dados[8];
-//			
-//			linhas[lin][col] = es;	
-//		}
-//		
-//	}
-//	while(dados.compare("0"));
-//	
-//	cout << "Informe o simbolo inicial: " << endl;	
-//	cin >> simboloIni;
-//	
-//	cout << "Informe a fita: " << endl;	
-//	cin >> fita;
-//	
-//	i = 0;
-//	ler('0', simboloIni);
-//	
-//}
-//
-//
-//main(){
-//int opcao=-1,nr=0;
-//	char temp='N';
-//	/*
-//	while(opcao != 9){
-//		system("cls");
-//		cout << endl;
-//		cout << "-------- Escolha uma opcao --------" << endl;cout << endl;
-//		cout << "1 - Soma" << endl;
-//		cout << "2 - Subtracao" << endl;
-//		cout << "3 - Divisao" << endl;
-//		cout << "4 - Multiplicacao" << endl;
-//		cout << "0 - Sair" << endl;
-//		cout <<endl;
-//	}*/
-//	
-//	entraDados();
-//	
-//	system("pause");
-//	return 0;
-//	
-//	
-//}
 
 
 
--- src/maquina.h
+++ src/maquina.h
@@ -8,24 +8,25 @@ using namespace std;
 
 class maquina{
    private:
-      int codigo;
+      int codigo, i = 0;
+      string fita;
       string nome;
+      char inicial, final;
       map<char, map<char, estado> > linhas;
-      void init();
+      void init(char est, char simbolo);
    
    public:
-      maquina();
-      void ler(vector<string> *dados);
+      maquina(vector<string> *dados, char inicial, char final);
+      void ler(string fita);
 };
 
-maquina::maquina(){}
-
-void maquina::ler(vector<string> *dados){
+maquina::maquina(vector<string> *dados, char inicial, char final) {
+	this->inicial = inicial;
+	this->final = final;
 	estado es;
 	char lin, col; 
   	for(int i = 0; i < dados->size(); i++) {
 	  	string dado = dados->at(i);
-	  	cout << "dado= "<< dado << endl;
 	  	lin = dado[0];
 		col = dado[2];
 		es.proximoEstado = dado[4];
@@ -35,6 +36,43 @@ void maquina::ler(vector<string> *dados){
 	}
 }
 
-void init() {
+void maquina::ler(string fita){
+	this->fita = fita;
+	this->init(this->inicial, this->inicial);
+}
+
+void maquina::init(char est, char simbolo) {
+	estado es = this->linhas[est][simbolo];
 	
+	if (es.proximoEstado == ' ')
+	{
+		cout << "Erro: estado nao encontrado";
+		return;
+	}
+	
+	cout << "est= " << est << "/" << simbolo << endl;
+	cout << "dir: " << es.proximoEstado << ',' << es.escreve << ',' << es.direcaoMovimento << endl;
+	
+	char proxSim;
+	
+	int anterior = this->i;
+	if(es.direcaoMovimento == 'D') {
+		proxSim = this->fita[++this->i];
+	} else {
+		proxSim = this->fita[--this->i];	
+	}
+	
+	this->fita[anterior] = es.escreve;
+	
+	
+	cout << "FITA: " << this->fita << endl;
+	
+	cout << "init: " << " = " << es.proximoEstado << '/' << proxSim << endl;
+	if (this->fita.empty() || es.proximoEstado == 'F')
+	{
+		cout << "Sucesso: fita lida com sucesso!" << endl;
+		return;
+	}
+	
+	this->init(es.proximoEstado, proxSim);	
 }
